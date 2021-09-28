3cb4f7a2ab2958ba9c4abe56e8c5758d9a84049a
Duy Nguyen <you@example.com>
2021-09-28 20:24:26 +0700

    fix

--- lesson_7_variable_data-type_operator/practice/obe.js
+++ lesson_7_variable_data-type_operator/practice/obe.js
@@ -1,8 +1,4 @@
-let inputWidth;
-let inputHeight;
-inputWidth = prompt("Enter the width");
-inputHeight = prompt("Enter the height");
-let width = parseInt(inputWidth);
-let height = parseInt(inputHeight);
+let width = parseInt(prompt("Enter the height"));
+let height = parseInt(prompt('Enter the width'));
 let area = width * height;
-document.write("The area is: " + area);
+alert("The area is: " + area);
