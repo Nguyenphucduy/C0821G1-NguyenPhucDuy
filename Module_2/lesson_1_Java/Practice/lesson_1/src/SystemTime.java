536c687cd87bbd609816ffaceedc66cec5b14f84
Duy Nguyen <you@example.com>
2021-10-01 10:29:44 +0700

    fix

--- Module_2/lesson_1_Java/Practice/lesson_1/src/SystemTime.java
+++
@@ -1,7 +0,0 @@
-public class SystemTime {
-    public static void main(String[] args) {
-        Date now = new Date(); // Lấy ra thời gian hiện tại
-
-        System.out.println("Now is: " + now);
-    }
-}
